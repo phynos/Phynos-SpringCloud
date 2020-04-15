11eca607db41be80863fab1adcb3e96b72f1a93e
phynos <phynos@126.com>
2020-04-15 21:29:16 +0800

    1

---
+++ phynos-auth/src/main/java/com/phynos/cloud/auth/AuthApplication.java
@@ -0,0 +1,23 @@
+package com.phynos.cloud.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author by lupc
+ * @date 2020-04-15 21:06
+ */
+@SpringBootApplication
+@RestController
+@EnableResourceServer
+@EnableAuthorizationServer
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
---
+++ phynos-monitor/src/main/java/com/phynos/cloud/monitor/MonitorApplication.java
@@ -0,0 +1,9 @@
+package com.phynos.cloud.monitor;
+
+/**
+ * @author by lupc
+ * @date 2020-04-15 21:05
+ */
+public class MonitorApplication {
+
+}
