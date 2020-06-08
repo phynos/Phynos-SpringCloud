4c5f7626e5c76d6b4ac8f7ed86afd976584d9e93
phynos <phynos@126.com>
2020-06-08 19:51:01 +0800

    1

---
+++ phynos-server-consumer/phynos-consumer-user/src/main/java/com/phynos/cloud/consumer/user/ConsumerUserApplication.java
@@ -0,0 +1,18 @@
+package com.phynos.cloud.consumer.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author by lupc
+ * @date 2020-06-08 16:30
+ */
+@SpringBootApplication
+public class ConsumerUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerUserApplication.class);
+    }
+
+
+}
