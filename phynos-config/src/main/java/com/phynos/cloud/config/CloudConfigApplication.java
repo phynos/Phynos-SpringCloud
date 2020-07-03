344e015f47ace064abe4c72294d88380ad88852e
phynos <phynos@126.com>
2020-07-03 14:55:56 +0800

    1

--- phynos-config/src/main/java/com/phynos/cloud/config/CloudConfigApplication.java
+++ phynos-config/src/main/java/com/phynos/cloud/config/CloudConfigApplication.java
@@ -3,12 +3,14 @@ package com.phynos.cloud.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author by lupc
  * @date 2020-04-13 11:15
  */
 @EnableConfigServer
+@EnableEurekaClient
 @SpringCloudApplication
 public class CloudConfigApplication {
 
