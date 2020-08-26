ee557883f06b5d1762e83f3a0dd12230fea9d466
phynos <phynos@126.com>
2020-08-26 10:57:18 +0800

    1

--- phynos-server-pruduct/phynos-product-sys/src/main/java/com/phynos/cloud/product/sys/ProductSysApplication.java
+++ phynos-server-pruduct/phynos-product-sys/src/main/java/com/phynos/cloud/product/sys/ProductSysApplication.java
@@ -1,8 +1,19 @@
 package com.phynos.cloud.product.sys;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * @author by lupc
  * @date 2020-08-21 10:28
  */
+@SpringBootApplication
+@EnableEurekaClient
 public class ProductSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductSysApplication.class, args);
+    }
+
 }
