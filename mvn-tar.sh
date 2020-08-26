e082134bf1fb87006af3bd4e51b789f75f248c94
phynos <phynos@126.com>
2020-08-26 21:11:39 +0800

    1

--- mvn-tar.sh
+++ mvn-tar.sh
@@ -2,16 +2,16 @@
 
 rm ./*.jar
 
-copy phynos-monitor/target/apps ./
-copy phynos-eureka/target/apps ./
-copy phynos-config/target/apps ./
-copy phynos-gateway/target/apps ./
-copy phynos-auth/target/apps ./
+cp phynos-monitor/target/apps ./
+cp phynos-eureka/target/apps ./
+cp phynos-config/target/apps ./
+cp phynos-gateway/target/apps ./
+cp phynos-auth/target/apps ./
 
 
-copy phynos-service-product/phynos-product-sys/target/apps ./
-copy phynos-service-product/phynos-product-user/target/apps ./
+cp phynos-service-product/phynos-product-sys/target/apps ./
+cp phynos-service-product/phynos-product-user/target/apps ./
 
-copy phynos-service-consumer/phynos-consumer-user/target/apps ./
+cp phynos-service-consumer/phynos-consumer-user/target/apps ./
 
 tar -czvf phynos-prod.tar.gz *.jar
