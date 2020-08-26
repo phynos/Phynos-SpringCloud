657a42d5d461819a9301c7b2a2a2bb4525adccf0
phynos <phynos@126.com>
2020-08-26 22:17:21 +0800

    1

--- startup.sh
+++ startup.sh
@@ -6,22 +6,23 @@ apps=(CloudMonitor CloudEureka CloudConfig CloudGateway CloudAuth2 CloudProductS
 for app in "${apps[@]}"
 do
   PID=`ps -ef|grep -v grep | grep $app| awk '{print $2}'`
-  if [-z $PID ];then
+  if [ $PID -eq 0 ];then
     echo 'app: '$app',pid: is empty'
   else
-    echo 'app: '$app',pid:'$PID
+    echo 'kill app: '$app',pid:'$PID
     kill -9 $PID
   fi
 
 done
 
-cd $PROGRAM
 
 rm -rf *.jar
-tar -xzvf phynos-prod.tar.gz
+tar -xzvf phynos-prod.tar.gz -C $PROGRAM
+
+cd $PROGRAM
 
 for app in "${apps[@]}"
 do
-	echo 'build: '$app
+	echo 'run: '$app
 	nohup java -jar $app.jar >/dev/null 2>&1 &
 done
