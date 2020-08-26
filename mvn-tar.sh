3474c1c9c610e198e2a3075f44387b1ae321e2bd
phynos <phynos@126.com>
2020-08-26 21:14:28 +0800

    1

--- mvn-tar.sh
+++ mvn-tar.sh
@@ -1,17 +1,18 @@
 
+apps=(CloudMonitor CloudEureka CloudConfig CloudGateway CloudAuth2 CloudProductSys CloudProductUser CloudConsumerUser)
 
-rm ./*.jar
+rm -rf ./*.jar
 
-cp phynos-monitor/target/apps ./
-cp phynos-eureka/target/apps ./
-cp phynos-config/target/apps ./
-cp phynos-gateway/target/apps ./
-cp phynos-auth/target/apps ./
+cp phynos-monitor/target/apps[0] ./
+cp phynos-eureka/target/apps[1] ./
+cp phynos-config/target/apps[2] ./
+cp phynos-gateway/target/apps[3] ./
+cp phynos-auth/target/apps[4] ./
 
 
-cp phynos-service-product/phynos-product-sys/target/apps ./
-cp phynos-service-product/phynos-product-user/target/apps ./
+cp phynos-service-product/phynos-product-sys/target/apps[5] ./
+cp phynos-service-product/phynos-product-user/target/apps[6] ./
 
-cp phynos-service-consumer/phynos-consumer-user/target/apps ./
+cp phynos-service-consumer/phynos-consumer-user/target/apps[7] ./
 
 tar -czvf phynos-prod.tar.gz *.jar
