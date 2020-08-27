828e1f38addd8700de4083a2cbe59f2eb2616738
phynos <phynos@126.com>
2020-08-27 13:51:46 +0800

    1

--- phynos-auth-oauth2/src/main/java/com/phynos/cloud/oauth2/OAuth2Application.java
+++ phynos-auth-oauth2/src/main/java/com/phynos/cloud/oauth2/OAuth2Application.java
@@ -1,4 +1,4 @@
-package com.phynos.cloud.auth;
+package com.phynos.cloud.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- phynos-auth-oauth2/src/main/java/com/phynos/cloud/oauth2/config/AuthorizationServerConfig.java
+++ phynos-auth-oauth2/src/main/java/com/phynos/cloud/oauth2/config/AuthorizationServerConfig.java
@@ -1,4 +1,4 @@
-package com.phynos.cloud.auth.config;
+package com.phynos.cloud.oauth2.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
