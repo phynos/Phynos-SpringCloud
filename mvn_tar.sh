e5f9c2678bafbb755583951ae261520ad5331449
phynos <phynos@126.com>
2020-09-16 14:48:41 +0800

    优化项目结构[F

--- mvn_tar.sh
+++ mvn_tar.sh
@@ -4,15 +4,15 @@ apps=(CloudMonitor CloudEureka CloudConfig CloudGateway CloudAuth2 CloudProductS
 rm -rf ./*.jar
 
 cp phynos-monitor/target/${apps[0]}.jar ./
-cp phynos-eureka/target/${apps[1]}.jar ./
-cp phynos-config/target/${apps[2]}.jar ./
-cp phynos-gateway/target/${apps[3]}.jar ./
+cp phynos-cloud-eureka/target/${apps[1]}.jar ./
+cp phynos-cloud-config/target/${apps[2]}.jar ./
+cp phynos-cloud-gateway/target/${apps[3]}.jar ./
 cp phynos-auth/target/${apps[4]}.jar ./
 
 
-cp phynos-server-product/phynos-product-sys/target/${apps[5]}.jar ./
-cp phynos-server-product/phynos-product-user/target/${apps[6]}.jar ./
+cp phynos-cloud-product/phynos-cloud-product-sys/target/${apps[5]}.jar ./
+cp phynos-cloud-product/phynos-cloud-product-user/target/${apps[6]}.jar ./
 
-cp phynos-server-consumer/phynos-consumer-user/target/${apps[7]}.jar ./
+cp phynos-cloud-consumer/phynos-cloud-consumer-user/target/${apps[7]}.jar ./
 
 tar -czvf phynos-prod.tar.gz *.jar
