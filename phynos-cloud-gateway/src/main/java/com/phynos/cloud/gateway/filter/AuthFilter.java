70cc96695e9e066fa2604927a43c972102128eec
phynos <phynos@126.com>
2020-10-12 09:46:22 +0800

    bom

--- phynos-cloud-gateway/src/main/java/com/phynos/cloud/gateway/filter/AuthFilter.java
+++ phynos-cloud-gateway/src/main/java/com/phynos/cloud/gateway/filter/AuthFilter.java
@@ -1,11 +1,40 @@
 package com.phynos.cloud.gateway.filter;
 
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
 /**
  * 网关鉴权
+ * <p>通过JWT判断请求是否携带合法的用户信息，并提取用户信息放入到head中</p>
+ *
  * @author by lupc
  * @date 2020-09-17 17:57
  */
-public class AuthFilter {
+@Component
+public class AuthFilter implements GlobalFilter, Ordered {
 
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String uri = exchange.getRequest().getURI().getPath();
+
+        boolean valid = uri.contains("/public");
+        if (valid) {
+            chain.filter(exchange);
+        } else {
+            //返回鉴权失败
+
+
+        }
+        return null;
+    }
+
+    @Override
+    public int getOrder() {
+        return -999;
+    }
 
 }
--- phynos-cloud-gateway/src/main/java/com/phynos/cloud/gateway/filter/BlackListUrlFilter.java
+++ phynos-cloud-gateway/src/main/java/com/phynos/cloud/gateway/filter/BlackListUrlFilter.java
@@ -1,8 +1,26 @@
 package com.phynos.cloud.gateway.filter;
 
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
 /**
  * @author by lupc
  * @date 2020-09-17 18:01
  */
-public class BlackListUrlFilter {
+public class BlackListUrlFilter implements GatewayFilter, Ordered {
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
 }
