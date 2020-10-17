6d936e8162df16ceab719c2a0fbf602fcc18fc3c
phynos <phynos@126.com>
2020-10-17 14:38:07 +0800

    1

--- phynos-cloud-gateway/src/main/java/com/phynos/cloud/gateway/filter/BlackListUrlFilter.java
+++ phynos-cloud-gateway/src/main/java/com/phynos/cloud/gateway/filter/BlackListUrlFilter.java
@@ -3,13 +3,16 @@ package com.phynos.cloud.gateway.filter;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 /**
+ * 黑名单过滤器
  * @author by lupc
  * @date 2020-09-17 18:01
  */
+@Component
 public class BlackListUrlFilter implements GatewayFilter, Ordered {
 
 
