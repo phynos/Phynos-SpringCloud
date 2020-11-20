4cbeb94b507f61020c7bf48796fb6e54d3551464
phynos <phynos@126.com>
2020-11-20 17:36:19 +0800

    调整目录

---
+++ charger-eureka/src/main/java/com/phynos/charger/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.phynos.charger.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author by lupc
+ * @date 2020-04-13 11:05
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
