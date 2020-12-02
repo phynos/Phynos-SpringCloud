9acd8ace3c5e32e7a52bd28227e23ec6e57c32d1
phynos <phynos@126.com>
2020-12-02 14:23:20 +0800

    1

--- charger-auth/src/main/java/com/phynos/charger/auth/AuthApplication.java
+++ charger-auth/src/main/java/com/phynos/charger/auth/AuthApplication.java
@@ -2,11 +2,13 @@ package com.phynos.charger.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author by lupc
  * @date 2020-04-15 21:06
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class AuthApplication {
 
