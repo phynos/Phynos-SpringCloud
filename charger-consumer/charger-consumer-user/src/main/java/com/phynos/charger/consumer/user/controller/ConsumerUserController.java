c96460c36713a4cea479655c5348163f3c7ff4c9
phynos <phynos@126.com>
2020-12-02 20:34:05 +0800

    11

--- charger-consumer/charger-consumer-user/src/main/java/com/phynos/charger/consumer/user/controller/ConsumerUserController.java
+++ charger-consumer/charger-consumer-user/src/main/java/com/phynos/charger/consumer/user/controller/ConsumerUserController.java
@@ -2,6 +2,7 @@ package com.phynos.charger.consumer.user.controller;
 
 import com.phynos.charger.consumer.user.remote.HelloRemote;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,15 @@ public class ConsumerUserController {
     @Autowired
     HelloRemote helloRemote;
 
+    @Autowired
+    ServerProperties serverProperties;
+
     @RequestMapping("/hello/{name}")
     public String index(@PathVariable("name") String name) {
-        return helloRemote.hello(name);
+        String port = serverProperties.getPort().toString();
+        String product = helloRemote.hello(name);
+        String result = String.format("consumer:port=%s;product:%s", port, product);
+        return result;
     }
 
 }
