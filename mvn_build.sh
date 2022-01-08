c861e5cf18337f924065f4b83ad7fffe31d0a67e
lupc <phynos@126.com>
2022-01-08 16:19:11 +0800

    简化

---
+++ mvn_build.sh
@@ -0,0 +1,47 @@
+
+PROOT=$(cd $(dirname $0); pwd);
+cd $PROOT
+
+dist=./dist
+mkdir -p dist
+
+echo start build...
+mvn clean package -am -Dmaven.test.skip=true
+
+echo clean files...
+rm -rf ${dist}/*.jar
+rm -rf ${dist}/*.gz
+
+function buildBaseApps() {
+    echo build base apps:
+    apps=(charger-monitor charger-eureka charger-config charger-gateway charger-auth)
+    for app in "${apps[@]}"
+    do
+      echo "build,app=${app}"
+      cp ${app}/target/${app}.jar $dist
+    done
+}
+
+function buildProduct() {
+    echo build product apps:
+    dir=charger-product
+    apps=(charger-product-sys charger-product-user)
+    for app in "${apps[@]}"
+    do
+      proj=$app
+      echo "build,app=${app}"
+      cp ${dir}/${proj}/target/${app}.jar $dist
+    done
+}
+
+function buildConsumer() {
+    echo build consumer apps:
+    dir=charger-consumer
+    apps=(charger-consumer-user)
+    for app in "${apps[@]}"
+    do
+      proj=$app
+      echo "build,app=${app}"
+      cp ${dir}/${proj}/target/${app}.jar $dist
+    done
+}
